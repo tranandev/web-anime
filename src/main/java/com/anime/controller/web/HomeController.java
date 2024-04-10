e1cd02a7b14fef37aa282869dde2c26116c29784
tranandev <trananwk@gmail.com>
2024-04-10 09:56:15 +0700

    add Controller

---
+++ src/main/java/com/anime/controller/web/HomeController.java
@@ -0,0 +1,28 @@
+package com.anime.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping(value = "/trang-chu", method = RequestMethod.GET)
+	public ModelAndView homePage() {
+		ModelAndView mav = new ModelAndView("web/home");
+		return mav;
+	}
+	
+	@RequestMapping(value = "/dang-nhap", method = RequestMethod.GET)
+	public ModelAndView loginPage() {
+		ModelAndView mav = new ModelAndView("login");
+		return mav;
+	}
+	
+	@RequestMapping(value = "/accessDenied", method = RequestMethod.GET)
+	public ModelAndView accessDenied() {
+		return new ModelAndView("redirect:/dang-nhap?accessDenied");
+	}
+
+}
