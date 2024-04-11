385483072f2d3a234744c89cf10ea2f3160bab2f
tranandev <trananwk@gmail.com>
2024-04-11 15:52:57 +0700

    support U-TF8

--- src/main/java/com/anime/utils/FormUtil.java
+++ src/main/java/com/anime/utils/FormUtil.java
@@ -1,5 +1,6 @@
 package com.anime.utils;
 
+import java.io.UnsupportedEncodingException;
 import java.lang.reflect.InvocationTargetException;
 
 import javax.servlet.http.HttpServletRequest;
@@ -12,9 +13,10 @@ public class FormUtil {
 	public static <T> T toModel(Class<T> clazz, HttpServletRequest request) {
 		T object = null;
 		try {
+			request.setCharacterEncoding("UTF-8");
 			object = clazz.newInstance();
 			BeanUtils.populate(object, request.getParameterMap());
-		} catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+		} catch (InstantiationException | IllegalAccessException | InvocationTargetException | UnsupportedEncodingException e) {
 			System.out.print(e.getMessage());
 		}
 		return object;
